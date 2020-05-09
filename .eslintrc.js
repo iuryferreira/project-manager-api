e4eb58af2a05bb52fc1aaa8bcb5a4e6e7a8c1c8a
Iury Ferreira <iury.franklinferreira@gmail.com>
2020-05-09 17:12:11 -0300

    🔧 configuring directory and start dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+    'linebreak-style': 0,
+  },
+};
